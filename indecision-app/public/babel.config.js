e03b64499ff9ff4470d53e57520318846d4967ea
Jose Ramón Bogarin Valenzuela <bogarin617@gmail.com>
2020-03-03 09:05:03 -0800

    cuatro

--- indecision-app/public/babel.config.js
+++ indecision-app/public/babel.config.js
@@ -1,1 +1,2 @@
 exports.presets = ["@babel/env", "@babel/preset-react"];
+exports.plugins = ["@babel/plugin-proposal-class-properties"];
---
+++ indecision-app/public/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'scrpits'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 4000
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ]
+};
